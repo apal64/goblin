c149b8b28ba08ae57dc667bdc418da1fce015f4f
apal64 <97973111+apal64@users.noreply.github.com>
2024-04-05 11:22:10 -0700

    Create userscreatedinlastweek.ps1

---
+++ userscreatedinlastweek.ps1
@@ -0,0 +1,6 @@
+#####Search AD for users created in the last week (populate the ou&dc)
+########variables
+
+$date = (get-Date).tostring() $week = (Get-Date).AddDays(-7) write-Host "Getting users created within a week." $ADuserInWeek = Get-ADUser -Filter 'name -notlike "test"' -Properties * -Searchbase “ou=,dc=“ | where { $_.whenCreated -ge $week } | select Name,whenCreated,UserPrincipalName,office,employeeid
+Search AD for computers created in the last week (populate ou&dc)
+variables
